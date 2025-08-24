d1bde9549eea4f7dd324e8c8523de31fa186a239
Tanishq Sonwane <tanishqsonwane32@gmail.com>
2025-08-24 23:19:20 +0530

    user register

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
-
-app.get('/',(req,res)=>{
-res.send('')
+const userModel = require('./models/user.model')
+app.use(express.json())
+app.post('/register',async (req,res)=>{
+const {email,password} = req.body
+ const user =  await userModel.create({
+    email,
+    password
+  })
+  res.send(user)
 }
 )
 
