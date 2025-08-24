bb18a394ec53ca7c8374d36c40d490ea1f02b671
Tanishq Sonwane <tanishqsonwane32@gmail.com>
2025-08-24 23:23:38 +0530

    token added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const userModel = require('./models/user.model')
+const jwt = require('jsonwebtoken')
 app.use(express.json())
 app.post('/register',async (req,res)=>{
 const {email,password} = req.body
@@ -8,7 +9,12 @@ const {email,password} = req.body
     email,
     password
   })
-  res.send(user)
+  const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+  res.cookie('token',token)
+  res.json({
+    user,
+    token
+  })
 }
 )
 
